3b1283548ed8e297cd16aab69c82508498f07d42
vsonu307 <73587457+vsonu307@users.noreply.github.com>
2023-01-10 03:08:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 import pickle

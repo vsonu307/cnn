82d30b2c5620a11c37efd50c24d1e01b812ccc53
vsonu307 <73587457+vsonu307@users.noreply.github.com>
2023-01-10 21:34:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,21 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
-import pickle
 import numpy as np
 
-
-
-pipe = pickle.load(open('model.pkl','rb'))
+model = load_model('NN')
 
 st.title('Image Classifier')
 input_image = st.file_uploader('Drop the image')
 
 
 if st.button('find'):
-    image = load_img(input_image, target_size=(50, 50))
+    image = load_img(input_image, target_size=(50, 50,3))
     img = img_to_array(image)
     img = np.array(img)
     img1 = img / 255.0
     img2 = img1.reshape(1, 50, 50, 3)
-    a = pipe.predict(img2)
+    a = model.predict(img2)
     if a<0.5:
         st.text('cat')
     else:
